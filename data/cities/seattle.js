c2ee170cadb2d6d1e07fca75533fce521c9958f3
ZbWylin115 <tjweagan@gmail.com>
2025-10-03 10:23:12 -0600

    Create seattle.js

---
+++ data/cities/seattle.js
@@ -0,0 +1,10 @@
+export const CITY_SEA={
+main:[
+{name:'M Bar (SLU)',desc:'Rooftop; Space Needle view.'},
+{name:'KBBQ & Karaoke (International District)',desc:'Food + singing; social fun.'},
+{name:'Pike Place Market (day)',desc:'Iconic casual daytime walk.'}
+],
+alt:[
+{name:'Oasis Tea Zone (ID)',desc:'Late-night boba staple.'},
+{name:'Dick’s Drive-In',desc:'Seattle classic post-club reset.'}
+]};
