5d5bcbc0fe5d7ea531d1a85e96a544ca991a9487
ZbWylin115 <tjweagan@gmail.com>
2025-10-03 10:22:47 -0600

    Create nyc.js

---
+++ data/cities/nyc.js
@@ -0,0 +1,10 @@
+export const CITY_NYC={
+main:[
+{name:'230 Fifth Rooftop (Flatiron)',desc:'Famous rooftop; Empire view.'},
+{name:'K-Town 32nd Street',desc:'K-BBQ + karaoke strip.'},
+{name:'Smorgasburg (Brooklyn)',desc:'Food festival vibe; artsy hub.'}
+],
+alt:[
+{name:'Spot Dessert Bar (EV)',desc:'Sweet pivot; playful plates.'},
+{name:'Prince Street Pizza',desc:'Iconic NY slice.'}
+]};
