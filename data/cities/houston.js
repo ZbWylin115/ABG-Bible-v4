c823ab10dfc6602474f51d273a5c1e01b6a11037
ZbWylin115 <tjweagan@gmail.com>
2025-10-03 10:22:17 -0600

    Create houston.js

---
+++ data/cities/houston.js
@@ -0,0 +1,10 @@
+export const CITY_HOUSTON={
+main:[
+{name:'Sekai Night & Day (EaDo)',desc:'Lounge/club hybrid; nightlife archetypes.'},
+{name:'Tiger Sugar (Chinatown)',desc:'Boba hub; casual pivot.'},
+{name:'Museum of Fine Arts',desc:'Artsy magnet; easy convo fuel.'}
+],
+alt:[
+{name:'The Pit Room / Truth BBQ',desc:'Texas BBQ flex; memorable.'},
+{name:'Late-night Cajun/crawfish',desc:'Messy, playful pivot.'}
+]};
